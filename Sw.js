0fee8e8c9fdfe387b4770a8a51f5cbf780643322
Pawan Jha <pjha2186@gmail.com>
2022-08-19 18:00:23 +0530

    fix

--- Sw.js
+++ Sw.js
@@ -1,20 +1,20 @@
-self.addEventListener("push", function (e) {
+self.addEventListener('push', function(e) {
   var options = {
     body: "This notification was generated from a push!",
     icon: "images/example.png",
     vibrate: [100, 50, 100],
     data: {
       dateOfArrival: Date.now(),
-      primaryKey: "2",
+      primaryKey: "2"
     },
     actions: [
       {
         action: "explore",
         title: "Explore this new world",
-        icon: "images/checkmark.png",
+        icon: "images/checkmark.png"
       },
-      { action: "close", title: "close", icon: "images/xmark.png" },
-    ],
+      { action: "Close", title: "Close", icon: "images/xmark.png" }
+    ]
   };
-  e.waitUntil(self.registeration.showNotification("Hello World!", options));
+  e.waitUntil(self.registration.Notification('Hello World!', options));
 });
--- push.js
+++ push.js
@@ -1,13 +1,17 @@
-var push = require('web-push');
+var push = require('web-push')
 
 let vapidKeys = {
-    publicKey: 'BHtESJfPHnErQmcEDEo2LfzNMOhq91F5BIMTG2kGhvqQkag_EfVg7ClT5vJEV5S2lD-Oy8U7jr2AQuv7BuHaIdk',
-    privateKey: 'gzhi-kJzlweHe0rkvCF-Se1kNYRyJoegKRJKkUR2V5U'
-  }
+  publicKey:
+    "BHtESJfPHnErQmcEDEo2LfzNMOhq91F5BIMTG2kGhvqQkag_EfVg7ClT5vJEV5S2lD-Oy8U7jr2AQuv7BuHaIdk",
+  privateKey: "gzhi-kJzlweHe0rkvCF-Se1kNYRyJoegKRJKkUR2V5U"
+};
 
+push.setVapidDetails(
+  "mailto:test@code.co.uk",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
 
-  push.setVapidDetails('mailto:test@code.co.uk',vapidKeys.publicKey, vapidKeys.privateKey)
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dzbDulPnARE:APA91bFNun0geSGhwZpiFR9UqFpyQTNeff51GIxvvL2BzbyBhbxIeXfBb6vGMoKZfSAQKxbB0WVKXyulDhMASDv6yvzpTQ_UMzXhSL3tq32k6PTLHkeffUcYAYFHsOIclubBuRYXksu-","expirationTime":null,"keys":{"p256dh":"BBkwCpKtglChu73KlcCi3hM9Ac-8VN09xpTy4bxJHEZOBWdLiWlWpWwF_hdVZ-HrmVzgce82-0lCIJZyFzC2Kpw","auth":"nID52nplPcXGPyiI-TDLaQ"}};
 
-  let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dzbDulPnARE:APA91bFNun0geSGhwZpiFR9UqFpyQTNeff51GIxvvL2BzbyBhbxIeXfBb6vGMoKZfSAQKxbB0WVKXyulDhMASDv6yvzpTQ_UMzXhSL3tq32k6PTLHkeffUcYAYFHsOIclubBuRYXksu-","expirationTime":null,"keys":{"p256dh":"BBkwCpKtglChu73KlcCi3hM9Ac-8VN09xpTy4bxJHEZOBWdLiWlWpWwF_hdVZ-HrmVzgce82-0lCIJZyFzC2Kpw","auth":"nID52nplPcXGPyiI-TDLaQ"}}
-
-  push.sendNotification(sub, 'test message')
+push.sendNotification(sub, "test message");
