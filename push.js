9d6314a8bd91fc163e85f5f2044cbdda683f9034
Pawan Jha <pjha2186@gmail.com>
2022-08-19 17:01:39 +0530

    fix

--- push.js
+++ push.js
@@ -8,6 +8,6 @@ let vapidKeys = {
 
   push.setVapidDetails('mailto:test@code.co.uk',vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub = {};
+  let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dzbDulPnARE:APA91bFNun0geSGhwZpiFR9UqFpyQTNeff51GIxvvL2BzbyBhbxIeXfBb6vGMoKZfSAQKxbB0WVKXyulDhMASDv6yvzpTQ_UMzXhSL3tq32k6PTLHkeffUcYAYFHsOIclubBuRYXksu-","expirationTime":null,"keys":{"p256dh":"BBkwCpKtglChu73KlcCi3hM9Ac-8VN09xpTy4bxJHEZOBWdLiWlWpWwF_hdVZ-HrmVzgce82-0lCIJZyFzC2Kpw","auth":"nID52nplPcXGPyiI-TDLaQ"}}
 
   push.sendNotification(sub, 'test message')
